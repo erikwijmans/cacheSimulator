e534f592969802f54a73759d1b81a5a0e3bd81d9
erikwijmans <ewijmans2@gmail.com>
2016-08-28 20:53:54 -0500

    xfer

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@
 git add .
 git commit -m "xfer"
 git push origin master
-ssh aws
-cd cacheSimulator && git pull && sudo apachectl restart
+ssh aws "cd cacheSimulator && git pull && sudo apachectl restart"
